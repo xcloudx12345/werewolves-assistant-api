6ddeb1706822dceecf928da7ed16d6efea1d6bce
xcloudx12345 <40192522+xcloudx12345@users.noreply.github.com>
2021-08-30 00:02:03 +0700

    Update index.js

--- config/migrate-mongo/index.js
+++ config/migrate-mongo/index.js
@@ -2,7 +2,7 @@ wconst Config = require("../index");
 
 module.exports = {
     mongodb: {
-        url: "mongodb+srv://xcloudx:<password>@cluster0.cstwg.mongodb.net/werewolf?retryWrites=true&w=majority",
+        url: "mongodb+srv://xcloudx:9627008@cluster0.cstwg.mongodb.net/werewolf?retryWrites=true&w=majority",
         databaseName: Config.db.name,
         options: {
             useNewUrlParser: true,
