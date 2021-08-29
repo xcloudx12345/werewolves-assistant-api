4d376572d47a9f90257eaf388eedbd57231e36c8
xcloudx12345 <40192522+xcloudx12345@users.noreply.github.com>
2021-08-29 23:28:13 +0700

    Update index.js

--- config/migrate-mongo/index.js
+++ config/migrate-mongo/index.js
@@ -2,7 +2,7 @@ const Config = require("../index");
 
 module.exports = {
     mongodb: {
-        url: "mongodb://localhost/",
+        url: "mongodb+srv://xcloudx:<password>@cluster0.cstwg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
         databaseName: Config.db.name,
         options: {
             useNewUrlParser: true,
